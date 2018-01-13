729d463877de1d227b0890196c07e42f667ba120
aditgege <aditgege0@gmail.com>
2018-01-13 18:38:40 +0700

    icon

--- Ngabensin/app/src/main/java/com/example/root/ngabensin/MainMenu.java
+++ Ngabensin/app/src/main/java/com/example/root/ngabensin/MainMenu.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
 
-    Button btnMyFuel;
+    Button btnMyFuel, btnFuelCost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +16,20 @@ public class MainMenu extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
 
         btnMyFuel = (Button)findViewById(R.id.btnMyFuel);
+        btnFuelCost = (Button)findViewById(R.id.btnFuelCost) ;
 
         btnMyFuel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainMenu.this,ChooseFuel.class);
+                Intent intent = new Intent(MainMenu.this,chose_fuel.class);
+                startActivity(intent);
+            }
+        });
+
+        btnFuelCost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainMenu.this,Check_FuelCost.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/root/ngabensin/MainMenu.java
+++ app/src/main/java/com/example/root/ngabensin/MainMenu.java
@@ -1,13 +1,34 @@
 package com.example.root.ngabensin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
-
+Button btnMyFuel , btnFuelCost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        btnMyFuel = (Button)findViewById(R.id.btnMyFuel);
+        btnMyFuel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainMenu.this,ChooseVehicle.class);
+                startActivity(intent);
+            }
+        });
+
+//        btnFuelCost = (Button)findViewById(R.id.btnFuelCost);
+//        btnFuelCost.setOnClickListener(new View.OnClickListener() {
+//            @Override
+////            public void onClick(View v) {
+////                Intent intent = new Intent(MainMenu.this,ChooseVehicle.class);
+////                startActivity(intent);
+////            }
+//        });
     }
 }
